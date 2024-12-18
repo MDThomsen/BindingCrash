5b13020b22c6918893c8bdab44189b6604d09bfa
MDThomsen <mdahlthomsen@hotmail.com>
2024-12-18 15:00:38 +0100

    Added project

---
+++ BindingCrash/BaseCrashingPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BindingCrash;
+
+public partial class BaseCrashingPage : ContentPage
+{
+	public virtual bool IsVisible => true;
+	public string Text => "Hello, World!";
+
+	public BaseCrashingPage()
+	{
+		InitializeComponent();
+	}
+}
---
+++ BindingCrash/CrashingPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System.Reflection;
+
+namespace BindingCrash;
+
+public class CrashingPage : BaseCrashingPage
+{
+	public override bool IsVisible => true;
+
+	public CrashingPage()
+	{
+		var visibleBindingPropertyInfo = GetType().GetTypeInfo().GetRuntimeProperty(nameof(IsVisible));
+
+		bool isVisible = false;
+
+		if (visibleBindingPropertyInfo != null)
+			isVisible = (bool)visibleBindingPropertyInfo.GetValue(this);
+
+		myEntry.IsVisible = isVisible;
+		myEntry.Text = Text;
+	}
+}
